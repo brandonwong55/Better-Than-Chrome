859d90cbcf35f687195cfdbfbd07723998b97bdc
brandonwong55 <35211716+brandonwong55@users.noreply.github.com>
2026-03-20 18:34:20 -0600

    Update user.js

--- user.js
+++ user.js
@@ -1,3 +1,6 @@
-user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-user_pref("svg.context-properties.content.enabled", true); // for curved_tabs.css
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.enabled", true);
+user_pref("layers.acceleration.force-enabled", true);
 user_pref("layout.css.color-mix.enabled", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
